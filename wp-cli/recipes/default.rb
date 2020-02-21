5243912498d4afc30c1889f612e49c542032bac5
EC2 Default User <ec2-user@ip-172-27-99-25.ap-northeast-1.compute.internal>
2020-02-21 05:55:26 +0000

    create wp-cli recipe2

--- wp-cli/recipes/default.rb
+++ wp-cli/recipes/default.rb
@@ -1,7 +1,7 @@
 wpdir = "/usr/bin/"
 
 remote_file "#{wpdir}/wp" do
-  source "https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar"
+  source "https://github.com/wp-cli/wp-cli/releases/download/v1.5.1/wp-cli-1.5.1.phar"
   owner "root"
   group "root"
   mode 00755
